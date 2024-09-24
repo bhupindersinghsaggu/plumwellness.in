137004fc131838ec8a8cd9b40bc7ad55e6ba2e15
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-09-24 18:30:36 +0530

    form added

--- web/footer.php
+++ web/footer.php
@@ -6,7 +6,9 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-6 col-xl-3">
                         <h4 class="footer-title pq-footer-link">About</h4>
-                        <p class="pq-footer-disc">we offer personalized nutrition coaching tailored to your unique needs and lifestyle. Our certified experts create sustainable, science-backed plans to help you achieve your health goals.</p>
+                        <p class="pq-footer-disc">we offer personalized nutrition coaching tailored to your unique needs
+                            and lifestyle. Our certified experts create sustainable, science-backed plans to help you
+                            achieve your health goals.</p>
                         <div class="pq-footer-social">
                             <ul>
                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -116,6 +118,44 @@
 <script src="js/isotope.pkgd.min.js"></script>
 <!--custom js-->
 <script src="js/custom.js"></script>
+
+<script>
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').trigger('focus')
+})
+</script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+
+<script>
+function sendwhatsapp() {
+    var phonenumber = "+919877800150";
+
+    var name = document.querySelector(".name").value;
+    var email = document.querySelector(".email").value;
+    var country = document.querySelector(".country").value;
+    var message = document.querySelector(".message").value;
+
+    var url = "https://wa.me/" + phonenumber + "?text=" +
+        "*Name :* " + name + "%0a" +
+        "*Email :* " + email + "%0a" +
+        "*Country:* " + country + "%0a" +
+        "*Message :* " + message +
+        "%0a%0a" +
+        "This is an example of send HTML form data to WhatsApp";
+
+    window.open(url, '_blank').focus();
+}
+</script>
+
 </body>
 
 <!-- <script>'undefined' === typeof _trfq || (window._trfq = []); 'undefined' === typeof _trfd && (window._trfd = []), _trfd.push({ 'tccl.baseHost': 'secureserver.net' }, { 'ap': 'cpbh-mt' }, { 'server': 'sg2plmcpnl492384' }, { 'dcenter': 'sg2' }, { 'cp_id': '9858662' }, { 'cp_cache': '' }, { 'cp_cl': '8' }) // Monitoring performance to make your website faster. If you want to opt-out, please contact web hosting support.</script>
--- web/header.php
+++ web/header.php
@@ -2,148 +2,188 @@
 <html lang="en">
 
 <head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>Plumwellness</title>
-   <!-- Favicon Icon -->
-   <link rel="shortcut icon" href="images/favicon.ico">
-   <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="css/bootstrap.min.css">
-   <!-- LOADING FONTS AND ICONS -->
-   <link rel="stylesheet" type="text/css" href="rev/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
-   <link rel="stylesheet" type="text/css" href="rev/fonts/font-awesome/css/font-awesome.css">
-   <!-- REVOLUTION STYLE SHEETS -->
-   <link rel="stylesheet" type="text/css" href="rev/css/rs6.css">
-   <!-- Icons CSS -->
-   <link rel="stylesheet" href="fonts/font-awesome/css/all.min.css">
-   <link rel="stylesheet" href="fonts/themify-icons/themify-icons.css">
-   <link rel="stylesheet" href="fonts/ionicons/ionicons.min.css">
-   <link rel="stylesheet" type="text/css" href="fonts/flaticons/flaticon_mycollection.css">
-   <!-- Owl Carousel CSS -->
-   <link rel="stylesheet" href="css/owl.carousel.min.css">
-   <!-- Magnefic Popup CSS -->
-   <link rel="stylesheet" href="css/magnific-popup.min.css">
-   <!-- Animation CSS -->
-   <link rel="stylesheet" href="css/animations.min.css">
-   <!--  Style CSS -->
-   <link rel="stylesheet" href="css/style.css">
-   <!--  Responsive CSS -->
-   <link rel="stylesheet" href="css/responsive.css">
-   <!--  custom CSS -->
-   <link rel="stylesheet" href="css/custom.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Plumwellness</title>
+    <!-- Favicon Icon -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- LOADING FONTS AND ICONS -->
+    <link rel="stylesheet" type="text/css" href="rev/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+    <link rel="stylesheet" type="text/css" href="rev/fonts/font-awesome/css/font-awesome.css">
+    <!-- REVOLUTION STYLE SHEETS -->
+    <link rel="stylesheet" type="text/css" href="rev/css/rs6.css">
+    <!-- Icons CSS -->
+    <link rel="stylesheet" href="fonts/font-awesome/css/all.min.css">
+    <link rel="stylesheet" href="fonts/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="fonts/ionicons/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/flaticons/flaticon_mycollection.css">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- Magnefic Popup CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.min.css">
+    <!-- Animation CSS -->
+    <link rel="stylesheet" href="css/animations.min.css">
+    <!--  Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!--  Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!--  custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
 </head>
 
 <body>
 
-   <!--Loading-->
-   <!-- <div id="pq-loading">
+    <!--Loading-->
+    <!-- <div id="pq-loading">
       <div id="pq-loading-center">
          <img src="images/header-logo/logo.png" alt="loading">
       </div>
    </div> -->
-   <!--Loading-->
+    <!--Loading-->
 
-   <!-- <header-start> -->
-   <header id="pq-header" class="pq-header-default">
-      <div class="pq-top-header">
-         <div class="container">
-            <div class="row flex-row-reverse">
-               <div class="col-md-6 text-right">
-                  <div class="pq-header-social text-right">
-                     <ul>
-                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                        <li><a href="#"><i class="fab fa-skype"></i></a></li>
-                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                     </ul>
-                  </div>
-               </div>
-               <div class="col-md-6">
-                  <div class="pq-header-contact ">
-                     <ul>
-                        <li>
-                           <a href="tel:+919988993972"><i class="fas fa-phone"></i>
-                              <span>+91 99889 93972</span>
-                           </a>
-                        </li>
-                        <li>
-                           <a href="mailto:Info@plumwellness.com"><i
-                                 class="fas fa-envelope"></i><span>info@plumwellness.com</span></a>
-                        </li>
-                     </ul>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <div class="pq-bottom-header">
-         <div class="container">
-            <div class="row">
-               <div class="col-lg-12">
-                  <nav class="navbar navbar-expand-lg navbar-light">
-                     <a class="navbar-brand" href="holozoic-nutrition.html">
-                        <img class="img-fluid logo" src="images/header-logo/logo.png" alt="nutritius">
-                     </a>
-                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div id="pq-menu-contain" class="pq-menu-contain">
-                           <ul id="pq-main-menu" class="navbar-nav ml-auto">
-                              <li class="menu-item current-menu-item">
-                                 <a href="index.php">Home</a>
-                              </li>
-                              <li class="menu-item ">
-                                 <a href="#">Service</a><i class="fa fa-chevron-down pq-submenu-icon"></i>
-                                 <ul class="sub-menu">
-                                    <li class="menu-item "><a href="weight-management.php ">Weight Management </a></li>
-                                    <li class="menu-item "><a href="disease-management.php">Disease Management</a></li>
-                                    <li class="menu-item "><a href="stress-management">Stress management</a></li>
-                                    <li class="menu-item "><a href="skin-hair-management.php">Skin & Hair Management</a>
-                                    </li>
-                                    <li class="menu-item "><a href="weight-loss-with-diet.php ">Weight Loss With Diet
-                                       </a></li>
-                                 </ul>
-                              </li>
-                              <li class="menu-item ">
-                                 <a href="#">Health Products</a>
-                              </li>
-                              </li>
-                              <li class="menu-item ">
-                                 <a href="#">About<i class="fa fa-chevron-down pq-submenu-icon"></a></i>
-                                 <ul class="sub-menu">
-                                    <li class="menu-item "><a href="weight-management.php ">About </a></li>
-                                    <li class="menu-item "><a href="disease-management.php">Team</a></li>
-                                 </ul>
-                              </li>
-                              <li class="menu-item "><a href="contact-us.html">Contact Us</a>
-                              </li>
-                           </ul>
+    <!-- <header-start> -->
+    <header id="pq-header" class="pq-header-default">
+        <div class="pq-top-header">
+            <div class="container">
+                <div class="row flex-row-reverse">
+                    <div class="col-md-6 text-right">
+                        <div class="pq-header-social text-right">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            </ul>
                         </div>
-                     </div>
-                     <div class="pq-menu-search-block">
-                        <a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss"><i
-                              class="fa-brands fa-whatsapp mgr"></i></a>
-                     </div>
-                     <div class="pq-button-container">
-                        <a class="pq-button pq-btn-flat" href="appointment.html">
-                           <span class="pq-btn-text">
-                              <img src="images/btn-img.svg" alt="svg-image">Appointment
-                           </span>
-                           <div class="pq-btn-cover">
-                              <span class="pq-btn-text pq-btn-cover-text">
-                                 <img src="images/btn-img.svg" alt="svg-image">Appointment
-                              </span>
-                              <div class="pq-btn-cover-bgrnd"></div>
-                           </div>
-                        </a>
-                     </div>
-                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fas fa-bars"></i>
-                     </button>
-                  </nav>
-               </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="pq-header-contact ">
+                            <ul>
+                                <li>
+                                    <a href="tel:+919988993972"><i class="fas fa-phone"></i>
+                                        <span>+91 99889 93972</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mailto:Info@plumwellness.com"><i
+                                            class="fas fa-envelope"></i><span>info@plumwellness.com</span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </header>
-   <!-- <header-end> -->
+        </div>
+        <div class="pq-bottom-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <a class="navbar-brand" href="holozoic-nutrition.html">
+                                <img class="img-fluid logo" src="images/header-logo/logo.png" alt="nutritius">
+                            </a>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <div id="pq-menu-contain" class="pq-menu-contain">
+                                    <ul id="pq-main-menu" class="navbar-nav ml-auto">
+                                        <li class="menu-item current-menu-item">
+                                            <a href="index.php">Home</a>
+                                        </li>
+                                        <li class="menu-item ">
+                                            <a href="#">Service</a><i class="fa fa-chevron-down pq-submenu-icon"></i>
+                                            <ul class="sub-menu">
+                                                <li class="menu-item "><a href="weight-management.php ">Weight
+                                                        Management </a></li>
+                                                <li class="menu-item "><a href="disease-management.php">Disease
+                                                        Management</a></li>
+                                                <li class="menu-item "><a href="stress-management">Stress management</a>
+                                                </li>
+                                                <li class="menu-item "><a href="skin-hair-management.php">Skin & Hair
+                                                        Management</a>
+                                                </li>
+                                                <li class="menu-item "><a href="weight-loss-with-diet.php ">Weight Loss
+                                                        With Diet
+                                                    </a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item ">
+                                            <a href="#">Health Products</a>
+                                        </li>
+                                        </li>
+                                        <li class="menu-item ">
+                                            <a href="#">About<i class="fa fa-chevron-down pq-submenu-icon"></a></i>
+                                            <ul class="sub-menu">
+                                                <li class="menu-item "><a href="weight-management.php ">About </a></li>
+                                                <li class="menu-item "><a href="disease-management.php">Team</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="menu-item "><a href="contact-us.html">Contact Us</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="pq-menu-search-block">
+                                <i class="fa-brands fa-whatsapp mgr" data-toggle="modal"
+                                    data-target="#exampleModal"></i>
+                            </div>
+                            <div class="pq-button-container">
+                                <button type="button" class="btn btn-primary" data-toggle="modal"
+                                    data-target="#exampleModal">
+                                    Launch demo modal
+                                </button>
+                            </div>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <i class="fas fa-bars"></i>
+                            </button>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- <header-end> -->
+
+
+    <!-- Button trigger modal -->
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="col-xl-12 col-lg-6 mt-4 mt-xl-0">
+                        <div class="appoinment-box">
+                            <form class="pq-applyform">
+                                <label for="">Name</label>
+                                <input type="text" class="name" class="form-control pq-bg-white paitent-name-field">
+
+                                <label for="">Email</label>
+                                <input type="text" class="email" class="form-control pq-bg-white phone-number-field">
+
+                                <label for="">Country</label>
+                                <input type="text" class="country" class="form-control pq-bg-white phone-number-field">
+
+                                <label for="">Message</label>
+                                <textarea class="message class=" form-control pq-bg-white
+                                    phone-number-field""></textarea>
+                                <button type="button" onclick="sendwhatsapp();">Send Via WhatsApp</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div> -->
+            </div>
+        </div>
+    </div>
