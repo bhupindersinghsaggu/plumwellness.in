13691796bb80a2c7a97885f57a9c8eb2f1e1cd4a
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-09-24 09:46:45 +0530

    convert o php

---
+++ web/about.php
@@ -0,0 +1,123 @@
+<!-- <about-counter-start> -->
+<section class="about-counter">
+   <div class="vector vector-1">
+      <img src="images/vector/6.png" alt="vector">
+   </div>
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-5 col-md-12 wow animated fadeInLeft">
+            <div class="pq-section-title-style-1">
+               <span class="pq-section-sub-title">about nutritius</span>
+               <h5 class="pq-section-title">Personalized coaching to help you cultivate a healthier lifestyle</h5>
+               <p class="pq-section-description">There are many variations of passages of Lorem Ipsum available, but
+                  the majority have suffered alteration in some form, by injected humour, or randomised words which
+                  don't look even slightly believable.</p>
+            </div>
+            <!-- <div class="pq-about-counter-container">
+                  <h6 class="about-counter-heading-title about-counter-size-default">Nutration Special Offers</h6>
+               </div> -->
+            <div class="row mt-3 mt-md-0">
+               <!-- <div class="col-lg-6 col-md-6">
+                     <ul class="pq-list-check ">
+                        <li><i aria-hidden="true" class="fas fa-arrow-circle-right"></i><span>Balance Body & Mind</span>
+                        </li>
+                        <li><i aria-hidden="true" class="fas fa-arrow-circle-right"></i><span>Personalized
+                              Nutrition</span></li>
+                        <li><i aria-hidden="true" class="fas fa-arrow-circle-right"></i><span>Child Nutrition</span>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="col-lg-6 col-md-6">
+                     <ul class="pq-list-check">
+                        <li><i aria-hidden="true" class="fas fa-arrow-circle-right"></i><span>Personal Coaching</span>
+                        </li>
+                        <li><i aria-hidden="true" class="fas fa-arrow-circle-right"></i><span>Sports Nutritionist</span>
+                        </li>
+                        <li><i aria-hidden="true" class="fas fa-arrow-circle-right"></i><span>Support &
+                              Motivation</span>
+                        </li>
+                     </ul>
+                  </div> -->
+               <div class="col-lg-12">
+                  <div class="pq-button-container pq-about-main-btn">
+                     <a class="pq-button pq-btn-flat" href="about-us.html">
+                        <span class="pq-btn-text">
+                           <img src="images/btn-img.svg" alt="svg-image">read more</span>
+                        <div class="pq-btn-cover">
+                           <span class="pq-btn-text pq-btn-cover-text">
+                              <img src="images/btn-img.svg" alt="svg-image">read more</span>
+                           <div class="pq-btn-cover-bgrnd"></div>
+                        </div>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-5 col-md-12 wow animated zoomIn">
+            <div class="pq-about-counter-img">
+               <img src="images/About-us/4.jpg" class="img-fluid" alt="about">
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-12 wow animated fadeInRight">
+            <div class="pq-about-counter-main-box">
+               <div class="pq-about-counter-box">
+                  <div class="pq-counter pq-style-2 text-center">
+                     <div class="pq-counter-contain ">
+                        <div class="pq-counter-info">
+                           <div class="pq-counter-num-prefix">
+                              <h5 class="timer" data-to="10" data-speed="5000">10</h5>
+                              <span class="pq-counter-prefix">+</span>
+                           </div>
+                           <h5 class="pq-counter-title">Years Experience</h5>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+               <div class="pq-about-counter-box mt-4 mt-lg-0">
+                  <div class="pq-counter pq-style-2 text-center">
+                     <div class="pq-counter-contain ">
+                        <div class="pq-counter-info">
+                           <div class="pq-counter-num-prefix">
+                              <h5 class="timer" data-to="368" data-speed="5000">368</h5>
+                              <span class="pq-counter-prefix">+</span>
+                           </div>
+                           <h5 class="pq-counter-title">Happy Customers</h5>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="pq-about-counter-box mt-4 mt-lg-0">
+                  <div class="pq-counter pq-style-2 text-center">
+                     <div class="pq-counter-contain ">
+                        <div class="pq-counter-info">
+                           <div class="pq-counter-num-prefix">
+                              <!-- <h5 class="timer" data-to="4" data-speed="1000">4</h5>
+                                 <span class="pq-counter-prefix">+</span> -->
+                              <img src="images/icons/trust.png">
+                           </div>
+                           <h5 class="pq-counter-title">TurstScore 4.0 | 47 Reviews</h5>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="pq-about-counter-box mt-4 mt-lg-0">
+                  <div class="pq-counter pq-style-2 text-center">
+                     <div class="pq-counter-contain ">
+                        <div class="pq-counter-info">
+                           <div class="pq-counter-num-prefix">
+                              <!-- <h5 class="timer" data-to="4" data-speed="1000">4</h5>
+                                 <span class="pq-counter-prefix">+</span> -->
+                              <img src="images/icons/google.png">
+                           </div>
+                           <h5 class="pq-counter-title">TurstScore 5.0 | 66 Reviews</h5>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- <about-counter-end> -->
