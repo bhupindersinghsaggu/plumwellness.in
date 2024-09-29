a9c637536acf6f94d88641f5ab518a6e52359389
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-09-29 13:19:08 +0530

    Update services.php

--- web/services.php
+++ web/services.php
@@ -1,101 +1,102 @@
 <!-- <service-section-start> -->
 <section class="service3 pq-services bg-purple">
-   <div class="container">
-      <div class="col-lg-12 wow  fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">
-         <div class="pq-section-title-style-1 text-center">
-            <h5 class="pq-section-title txt-white">Our Services</h5>
-         </div>
-      </div>
-      <div class="row">
+    <div class="container">
+        <div class="col-lg-12 wow  fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">
+            <div class="pq-section-title-style-1 text-center">
+                <h5 class="pq-section-title txt-white">Our Services</h5>
+            </div>
+        </div>
+        <div class="row">
 
-         <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
-            style="visibility: visible; animation-name: fadeInUp;">
-            <div class="pq-icon-box pq-style-2 txt-center">
-               <div class="pq-icon">
-                  <a href="pcod.php"><img src="images/icons/pcod.png" class="icon-pd"></a>
-               </div>
-               <div class="pq-icon-box-content mb-30">
-                  <h6 class="service-txt">PCOS </h6>
-                  <p class="pq-icon-box-description">
-                     Break free from PCOS challenges and embrace a healthier you. Reclaim your vitality, unlock your
-                     potential, and start your wellness journey today!
-                  </p>
-                  <div class="pq-button-container">
-                     <a class="pq-button pq-btn-link" href="pcos.php">
-                        <span class="pq-btn-text">Know More</span>
-                     </a>
-                  </div>
-                  <!-- <div class="ser-btn d-flex justify-content-center"> <a href="pcos.php" class="
+            <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="pq-icon-box pq-style-2 txt-center">
+                    <div class="pq-icon">
+                        <a href="pcod.php"><img src="images/icons/pcod.png" class="icon-pd"></a>
+                    </div>
+                    <div class="pq-icon-box-content mb-30">
+                        <h6 class="service-txt">Disease Management </h6>
+                        <p class="pq-icon-box-description">
+                            We provide personalized, comprehensive disease management services to improve your health.
+                            Whether you're managing a chronic condition or recovering from illness, our healthcare team
+                            is here to support you at every step.
+                        </p>
+                        <div class="pq-button-container">
+                            <a class="pq-button pq-btn-link" href="pcos.php">
+                                <span class="pq-btn-text">Know More</span>
+                            </a>
+                        </div>
+                        <!-- <div class="ser-btn d-flex justify-content-center"> <a href="pcos.php" class="
                         secondary-btn md-btn bott0n-border"> Know More</a>
                      </div> -->
-               </div>
+                    </div>
+                </div>
             </div>
-         </div>
-         <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
-            style="visibility: visible; animation-name: fadeInUp;">
-            <div class="pq-icon-box pq-style-2 txt-center">
-               <div class="pq-icon">
-                  <a href="thyroid.php"><img src="images/icons/thyroid.png" class="icon-pd"></a>
-               </div>
-               <div class="pq-icon-box-content">
-                  <h6 class="service-txt">Thyroid </h6>
-                  <p class="pq-icon-box-description">Take control of your thyroid disorder to lose weight
-                     effectively. Understand your condition and use the right strategies to manage your health.
-                     Embrace this journey to empower yourself and achieve your wellness goals.</p>
-                  <div class="pq-button-container mb-30">
-                     <a class="pq-button pq-btn-link" href="thyroid.php">
-                        <span class="pq-btn-text">Know More</span>
-                     </a>
-                  </div>
-                  <!-- <div class="ser-btn d-flex justify-content-center"> <a href="thyroid.php" class="
+            <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="pq-icon-box pq-style-2 txt-center">
+                    <div class="pq-icon">
+                        <a href="thyroid.php"><img src="images/icons/thyroid.png" class="icon-pd"></a>
+                    </div>
+                    <div class="pq-icon-box-content">
+                        <h6 class="service-txt">Thyroid </h6>
+                        <p class="pq-icon-box-description">Take control of your thyroid disorder to lose weight
+                            effectively. Understand your condition and use the right strategies to manage your health.
+                            Embrace this journey to empower yourself and achieve your wellness goals.</p>
+                        <div class="pq-button-container mb-30">
+                            <a class="pq-button pq-btn-link" href="thyroid.php">
+                                <span class="pq-btn-text">Know More</span>
+                            </a>
+                        </div>
+                        <!-- <div class="ser-btn d-flex justify-content-center"> <a href="thyroid.php" class="
                            secondary-btn md-btn bott0n-border"> Know More</a>
                      </div> -->
-               </div>
+                    </div>
+                </div>
             </div>
-         </div>
-         <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
-            style="visibility: visible; animation-name: fadeInUp;">
-            <div class="pq-icon-box pq-style-2 txt-center">
-               <div class="pq-icon">
-                  <a href="diabetes.php"><img src="images/icons/blood-test.png" class="icon-pd"></a>
-               </div>
-               <div class="pq-icon-box-content">
-                  <h6 class="service-txt">Diabetes </h6>
-                  <p class="pq-icon-box-description">Manage your diabetes with confidence by learning about your
-                     condition and applying the right strategies to reach your health and wellness goals.</p>
-                  <div class="pq-button-container mb-30">
-                     <a class="pq-button pq-btn-link" href="diabetes.php">
-                        <span class="pq-btn-text">Know More</span>
-                     </a>
-                  </div>
-                  <!-- <div class="ser-btn d-flex justify-content-center"> <a href="diabetes.php" class="
+            <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="pq-icon-box pq-style-2 txt-center">
+                    <div class="pq-icon">
+                        <a href="diabetes.php"><img src="images/icons/blood-test.png" class="icon-pd"></a>
+                    </div>
+                    <div class="pq-icon-box-content">
+                        <h6 class="service-txt">Diabetes </h6>
+                        <p class="pq-icon-box-description">Manage your diabetes with confidence by learning about your
+                            condition and applying the right strategies to reach your health and wellness goals.</p>
+                        <div class="pq-button-container mb-30">
+                            <a class="pq-button pq-btn-link" href="diabetes.php">
+                                <span class="pq-btn-text">Know More</span>
+                            </a>
+                        </div>
+                        <!-- <div class="ser-btn d-flex justify-content-center"> <a href="diabetes.php" class="
                            secondary-btn md-btn bott0n-border"> Know More</a> -->
-               </div>
+                    </div>
+                </div>
             </div>
-         </div>
-         <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
-            style="visibility: visible; animation-name: fadeInUp;">
-            <div class="pq-icon-box pq-style-2 txt-center">
-               <div class="pq-icon">
-                  <a href="thyroid.php"><img src="images/icons/hypertension.png" class="icon-pd"></a>
-               </div>
-               <div class="pq-icon-box-content">
-                  <h6 class="service-txt">Hypertension </h6>
-                  <p class="pq-icon-box-description">Take control of your hypertension by understanding your
-                     condition and implementing effective strategies. With informed choices and simple lifestyle
-                     changes, you can manage your health and achieve lasting wellness</p>
-                  <div class="pq-button-container mb-30">
-                     <a class="pq-button pq-btn-link" href="diabetes.php">
-                        <span class="pq-btn-text">Know More</span>
-                     </a>
-                  </div>
-                  <!-- <div class="ser-btn d-flex justify-content-center"> <a href="diabetes.php" class="
+            <div class="col-xl-3 col-lg-6 col-md-6 wow  fadeInUp animated"
+                style="visibility: visible; animation-name: fadeInUp;">
+                <div class="pq-icon-box pq-style-2 txt-center">
+                    <div class="pq-icon">
+                        <a href="thyroid.php"><img src="images/icons/hypertension.png" class="icon-pd"></a>
+                    </div>
+                    <div class="pq-icon-box-content">
+                        <h6 class="service-txt">Hypertension </h6>
+                        <p class="pq-icon-box-description">Take control of your hypertension by understanding your
+                            condition and implementing effective strategies. With informed choices and simple lifestyle
+                            changes, you can manage your health and achieve lasting wellness</p>
+                        <div class="pq-button-container mb-30">
+                            <a class="pq-button pq-btn-link" href="diabetes.php">
+                                <span class="pq-btn-text">Know More</span>
+                            </a>
+                        </div>
+                        <!-- <div class="ser-btn d-flex justify-content-center"> <a href="diabetes.php" class="
                            secondary-btn md-btn bott0n-border"> Know More</a>
                      </div> -->
-               </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </section>
 <!-- <service-section-end> -->
