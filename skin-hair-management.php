3c887927315097d74e9fb4d2d47520df36f747a8
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-10-02 17:17:00 +0530

    skin care

--- skin-hair-management.php
+++ skin-hair-management.php
@@ -9,8 +9,8 @@ include('web/header.php');
             <div class="col-xl-6 pe-xl-5 pq-process-main wow  fadeInLeft animated"
                 style="visibility: visible; animation-name: fadeInLeft;">
                 <div class="pq-section-title-style-1">
-                    <h5 class="pq-section-title mb-30 ">Rejuvenate Your <span class="span-color">Skin and Hair  </span>Naturally – Start Today!
-                        </h5>
+                    <h5 class="pq-section-title mb-30 ">Rejuvenate Your <span class="span-color">Skin and Hair </span>Naturally – Start Today!
+                    </h5>
                     <p class="pq-fancy-box-title slider-sub">Nourishing solutions tailored to your unique needs. Feel confident in your natural beauty with our premium, science-backed care</p>
                 </div>
                 <ul class="banner-cta flex ul-list-hero mrl-20">
@@ -22,7 +22,7 @@ include('web/header.php');
             </div>
             <div class="col-xl-6 wow  fadeInRight animated" style="visibility: visible; animation-name: fadeInRight;">
                 <div class="pq-progressbar-img">
-                    <img src="images/main22.jpg" class="img-fluid" alt="images">
+                    <img src="images/main3.jpg" class="img-fluid" alt="images">
                 </div>
             </div>
         </div>
@@ -33,37 +33,91 @@ include('web/header.php');
         <div class="row">
             <div class="col-lg-12 wow  fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">
                 <div class="pq-section-title-style-1 text-center">
-                    <h5 class="pq-section-title">Tried various treatments but haven't found relief?</h5>
-                    <p class="pq-fancy-box-title slider-sub">At Plumwellness, we believe in personalized care. Our
-                        integrated, evidence-based
-                        model is tailored to your unique needs, helping you and your family navigate depression and
-                        recover more quickly.</p>
+                    <h5 class="pq-section-title">Tried countless skincare or hair treatments but still haven't found relief?</h5>
+                    <p class="pq-fancy-box-title slider-sub">At Plumwellness, we prioritize personalized care. Our integrated, evidence-based approach is designed to meet your unique skincare and hair care needs, helping you achieve lasting results and restoring your confidence.
+                    </p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xl-4 col-lg-6 col-md-6">
                     <div class="pq-class-box-1">
                         <div class="pq-class-box-media">
-                            <img decoding="async" src="images/services/1.jpg" alt="image">
+                            <img decoding="async" src="images/services/hair.jpg" alt="image">
                         </div>
                         <div class="pq-class-box-info">
-                            <h5 class="pq-class-box-title">Comprehensive & holistic care</h5>
+                            <h5 class="pq-class-box-title">Hair Transplant</h5>
                             <p class="pq-class-box-description">
-                                Our psychiatrists combine medication, therapy, and lifestyle changes for a comprehensive
-                                mental health approach.</p>
+                                Achieve natural-looking hair with an affordable hair transplant. Enjoy outstanding results, exceptional customer service, and the satisfaction of countless happy patients. Contact us to learn more about hair transplants.
+                            <ul class="banner-cta flex ul-list-hero">
+                                <li><a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss" class="
+                                primary-btn1 md-btn" data-toggle="modal" data-target="#exampleModal"> <i class="fa-brands fa-whatsapp mgr"></i> Talk to an Expert</a></li>
+                            </ul>
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-md-6">
                     <div class="pq-class-box-1">
                         <div class="pq-class-box-media">
-                            <img decoding="async" src="images/services/2.jpg" alt="image">
+                            <img decoding="async" src="images/services/hair-remove.jpg" alt="image">
                         </div>
                         <div class="pq-class-box-info">
-                            <h5 class="pq-class-box-title">Customized treatment plans</h5>
+                            <h5 class="pq-class-box-title">Laser Hair Removal </h5>
                             <p class="pq-class-box-description">
-                                We know everyone’s needs are unique, so we customize each treatment plan to fit your
-                                specific goals and preferences</p>
+                                Laser hair removal has been a game-changer, offering relief from the exhausting and tedious routine of shaving and waxing. It is now one of the most popular and effective cosmetic procedures for permanently eliminating unwanted hair.
+                            <ul class="banner-cta flex ul-list-hero">
+                                <li><a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss" class="
+                                primary-btn1 md-btn" data-toggle="modal" data-target="#exampleModal"> <i class="fa-brands fa-whatsapp mgr"></i> Talk to an Expert</a></li>
+                            </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6">
+                    <div class="pq-class-box-1">
+                        <div class="pq-class-box-media">
+                            <img decoding="async" src="images/services/chemical.jpg" alt="image">
+                        </div>
+                        <div class="pq-class-box-info">
+                            <h5 class="pq-class-box-title">Chemical Peel</h5>
+                            <p class="pq-class-box-description">Chemical peels effectively reduce fine lines, wrinkles, and sun damage while improving skin texture. Contact us to learn more about this treatment.
+                            <ul class="banner-cta flex ul-list-hero">
+                                <li><a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss" class="
+                            primary-btn1 md-btn" data-toggle="modal" data-target="#exampleModal"> <i class="fa-brands fa-whatsapp mgr"></i> Talk to an Expert</a></li>
+                            </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6">
+                    <div class="pq-class-box-1">
+                        <div class="pq-class-box-media">
+                            <img decoding="async" src="images/services/anti.jpg" alt="image">
+                        </div>
+                        <div class="pq-class-box-info">
+                            <h5 class="pq-class-box-title">Anti Aging</h5>
+                            <p class="pq-class-box-description">Discover advanced anti-aging treatments to help you look your best at any age. Click here to learn more about our luxurious anti-aging solutions.
+                            <ul class="banner-cta flex ul-list-hero">
+                                <li><a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss" class="
+                                primary-btn1 md-btn" data-toggle="modal" data-target="#exampleModal"> <i class="fa-brands fa-whatsapp mgr"></i> Talk to an Expert</a></li>
+                            </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-md-6">
+                    <div class="pq-class-box-1">
+                        <div class="pq-class-box-media">
+                            <img decoding="async" src="images/services/pre.jpg" alt="image">
+                        </div>
+                        <div class="pq-class-box-info">
+                            <h5 class="pq-class-box-title">PRP Treatment</h5>
+                            <p class="pq-class-box-description">Hair fall has become a common issue for almost everyone these days. You’ve likely tried various remedies to improve your hair, but you know firsthand how effective the results truly are
+                            <ul class="banner-cta flex ul-list-hero">
+                                <li><a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss" class="
+                                primary-btn1 md-btn" data-toggle="modal" data-target="#exampleModal"> <i class="fa-brands fa-whatsapp mgr"></i> Talk to an Expert</a></li>
+                            </ul>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -73,21 +127,25 @@ include('web/header.php');
                             <img decoding="async" src="images/services/3.jpg" alt="image">
                         </div>
                         <div class="pq-class-box-info">
-                            <h5 class="pq-class-box-title">Focused on thorough diagnosis</h5>
-                            <p class="pq-class-box-description">Our focus on diagnostics enables accurate assessments
-                                and targeted treatment plans.</p>
+                            <h5 class="pq-class-box-title">Skin tightening </h5>
+                            <p class="pq-class-box-description">Nonsurgical skin tightening treatments enhance the appearance of your face with far less invasiveness than surgical options like facelifts. Contact us to know more about the procedure
+                            <ul class="banner-cta flex ul-list-hero">
+                                <li><a href="https://wa.me/919988993972?text=I%20want%20to%20know%20more%20about%20weight%20loss" class="
+                            primary-btn1 md-btn" data-toggle="modal" data-target="#exampleModal"> <i class="fa-brands fa-whatsapp mgr"></i> Talk to an Expert</a></li>
+                            </ul>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row mt-30 ">
+            <!-- <div class="row mt-30 ">
                 <div class="pq-button-container d-flex justify-content-center">
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                         Book an Appoinment
                     </button>
                 </div>
-            </div>
-            <div class="row mt-30 ">
+            </div> -->
+            <!-- <div class="row mt-30 ">
                 <div class="pq-faq2">
                     <div class="pq-faq2-info">
                         <div class="col-lg-12 wow  fadeInDown animated" style="visibility: visible; animation-name: fadeInDown;">
@@ -219,7 +277,7 @@ include('web/header.php');
                                         <div class="col-sm-4 mt-30 mb-30"><img decoding="async" src="images/services/7.jpeg" alt="image"></div>
                                         <div class="col-sm-8">
                                             <p class="pq-detail-text"> The exact cause of bipolar disorder is not fully understood, but it includes a combination of genetic, biological, and environmental factors. Imbalances in brain chemicals and changes in the structure or function of the brain may also play a role.</p>
-                                            <!-- <h5 class="pq-class-box-title mb-30 mt-30">Our comprehensive treatments include:</h5> -->
+                                           <h5 class="pq-class-box-title mb-30 mt-30">Our comprehensive treatments include:</h5> 
                                             <ul class="banner-cta flex ul-list-hero mt-30">
                                                 <li><a href="tel:91+919988993972" class="primary-btn1 md-btn"> <i class="fa-solid fa-phone mgr"></i>
                                                         Talk to us</a></li>
@@ -270,7 +328,7 @@ include('web/header.php');
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         </div>
     </div>
 </section>
