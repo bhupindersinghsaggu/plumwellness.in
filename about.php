831e1247d90393b29b38e42f10c580ef4f6fa43c
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-10-10 10:28:31 +0530

    sdfsfd

---
+++ about.php
@@ -0,0 +1,42 @@
+<?php
+include('web/header.php');
+// include('web/hero-section-stress.php');
+?>
+<section class="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-xl-6 pe-xl-5">
+                <div class="pq-about-us-img1">
+                    <img src="images/About-us/2.jpg" class="img-fluid pq-image2" alt="about">
+                    <img src="images/skill/1.jpg" class="img-fluid pq-image3" alt="about">
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-xl-6 pq-about-contain">
+                <div class="pq-section-title-style-1 ">
+                    <h5 class="pq-section-title">ABOUT US</h5>
+                    <p class="pq-section-description">Welcome to Plum Wellness, your trusted online destination for diet
+                        and wellness solutions. With a decade of experience, we have been dedicated to empowering
+                        individuals to achieve their health goals through effective, personalized strategies. Our
+                        mission is to provide comprehensive support for diet, weight loss, stress management, and
+                        overall well-being.
+                    </p>
+
+                    <p class="pq-section-description">At Plum Wellness, we understand that every individual is unique.
+                        That’s why we offer tailored
+                        programs that address your specific needs and lifestyle. Whether you’re looking to shed a few
+                        pounds, manage stress, or enhance your overall health, our expert team is here to guide you on
+                        your journey. Our approach combines the latest research in nutrition with practical,
+                        easy-to-implement strategies that fit seamlessly into your daily routine.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row g-0">
+
+        </div>
+    </div>
+</section>
+
+<?php
+include('web/footer.php');
+// include('web/hero-section-stress.php');
+?>
