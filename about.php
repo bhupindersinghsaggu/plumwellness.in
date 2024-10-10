73ef5ad9a6de31540de2b8b68c380c8dc459ef0c
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-10-10 18:54:56 +0530

    sdfsdf

--- about.php
+++ about.php
@@ -4,11 +4,10 @@ include('web/header.php');
 ?>
 <section class="about-us">
     <div class="container">
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-lg-12 col-md-12 col-xl-6 pe-xl-5">
-                <div class="pq-about-us-img1">
+                <div class="">
                     <img src="images/About-us/2.jpg" class="img-fluid pq-image2" alt="about">
-                    <img src="images/skill/1.jpg" class="img-fluid pq-image3" alt="about">
                 </div>
             </div>
             <div class="col-lg-12 col-md-12 col-xl-6 pq-about-contain">
@@ -42,7 +41,11 @@ include('web/header.php');
             <div class="col-lg-12 col-md-12 col-xl-4 pq-project-description">
                 <div class="pq-section-title-style-1 ">
                     <h5 class="pq-section-title">Our Team</h5>
-                    <p class="pq-section-description">Our team comprises seasoned professionals, including experienced doctors and registered dietitians, all committed to your success. Together, we have helped over 10,000 individuals transform their lives through our evidence-based methodologies. We pride ourselves on fostering a supportive and inclusive environment, where your health and wellness goals are our top priority.</p>
+                    <p class="pq-section-description">Our team comprises seasoned professionals, including experienced
+                        doctors and registered dietitians, all committed to your success. Together, we have helped over
+                        10,000 individuals transform their lives through our evidence-based methodologies. We pride
+                        ourselves on fostering a supportive and inclusive environment, where your health and wellness
+                        goals are our top priority.</p>
                 </div>
                 <!-- <div class="pq-button-container pq-about-main-btn">
                     <a class="pq-button pq-btn-flat" href="portfolio-single.html">
@@ -59,9 +62,9 @@ include('web/header.php');
             <div class="col-lg-12 col-md-12 col-xl-8 p-0 pq-mt-60">
                 <div class="pq-main-portfoliobox-1 pq-bg-primary">
                     <div class="pq-main-images-box">
-                        <div class="owl-carousel owl-loaded owl-drag" data-dots="false" data-nav="false" data-desk_num="3"
-                            data-lap_num="3" data-tab_num="2" data-mob_num="1" data-mob_sm="1" data-autoplay="true"
-                            data-loop="true" data-margin="30">
+                        <div class="owl-carousel owl-loaded owl-drag" data-dots="false" data-nav="false"
+                            data-desk_num="3" data-lap_num="3" data-tab_num="2" data-mob_num="1" data-mob_sm="1"
+                            data-autoplay="true" data-loop="true" data-margin="30">
                             <div class="item">
                                 <div class="pq-portfoliobox-1">
                                     <div class="pq-portfolio-block">
@@ -71,7 +74,8 @@ include('web/header.php');
                                         <div class="pq-portfolio-info">
                                             <h5 class="pq-blog-title txt-center">Dt. Miksha Arora</h5>
                                             <div class="pq-testimonial-meta txt-center">
-                                                <h6 class="ad-title-text">Weight Loss Expert</h6> <span>10+ Years of Experience</span>
+                                                <h6 class="ad-title-text">Weight Loss Expert</h6> <span>10+ Years of
+                                                    Experience</span>
                                             </div>
                                         </div>
                                     </div>
@@ -86,7 +90,8 @@ include('web/header.php');
                                         <div class="pq-portfolio-info">
                                             <h5 class="pq-blog-title txt-center">Dt. Bhawna </h5>
                                             <div class="pq-testimonial-meta txt-center">
-                                                <h6 class="ad-title-text">Disease Management Expert</h6><span>5+ Years of
+                                                <h6 class="ad-title-text">Disease Management Expert</h6><span>5+ Years
+                                                    of
                                                     Experience</span>
                                             </div>
                                         </div>
@@ -118,7 +123,8 @@ include('web/header.php');
                                         <div class="pq-portfolio-info">
                                             <h5 class="pq-blog-title txt-center">Dt. Akansha </h5>
                                             <div class="pq-testimonial-meta txt-center">
-                                                <h6 class="ad-title-text">Weight Loss Expert</h6><span>8+ Years of Experience</span>
+                                                <h6 class="ad-title-text">Weight Loss Expert</h6><span>8+ Years of
+                                                    Experience</span>
                                             </div>
                                         </div>
                                     </div>
@@ -132,7 +138,90 @@ include('web/header.php');
     </div>
 </section>
 <!-- <project-end> -->
+<section class="Project p-0">
+    <div class="container-fluid p-0">
+        <div class="row g-0">
+            <div class="col-xl-6 wow  fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft;">
+                <div class="row g-0">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="pq-project-img1">
+                            <!-- <img src="images/1.jpg" class="img-fluid" alt="project"> -->
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="pq-project-detail-main-box">
+                            <div class="pq-fancy-box-4 pq-fancy-bg-grey text-center">
+                                <div class="pq-fancy-box-info">
+                                    <h5 class="pq-fancy-box-title">Holistic Health</h5>
+                                    <div class="pq-fancy-box-content">
+                                        <p class="pq-fancybox-description">Plum Wellness stands out for its holistic
+                                            approach to health. We believe that wellness is not just about diet; it
+                                            encompasses physical, mental, and emotional well-being. Our programs are
+                                            designed to help you cultivate a balanced lifestyle that promotes long-term
+                                            health. From personalized meal plans to stress reduction techniques, we
+                                            provide the tools and resources necessary to thrive.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pq-fancy-box-4 text-center pq-fancy-bg-color">
 
+                                <div class="pq-fancy-box-info">
+
+                                    <h5 class="pq-fancy-box-title">Access Anytime, Anywhere</h5>
+                                    <div class="pq-fancy-box-content">
+                                        <p class="pq-fancybox-description">Our online platform makes it easy for you to
+                                            access our services anytime, anywhere. Whether you prefer one-on-one
+                                            consultations or group sessions, we offer flexibility to suit your schedule.
+                                            With our user-friendly resources, including informative articles, meal
+                                            guides, and wellness tips, you will have everything you need at your
+                                            fingertips.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6 wow  fadeInRight animated" style="visibility: visible; animation-name: fadeInRight;">
+                <div class="row g-0 flex-row-reverse flex-xl-row">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="pq-project-img2">
+                            <!-- <img src="images/2.jpg" class="img-fluid" alt="project"> -->
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="pq-project-detail-main-box">
+                            <div class="pq-fancy-box-4 text-center pq-fancy-bg-color">
+                                <div class="pq-fancy-box-info">
+                                    <h5 class="pq-fancy-box-title">Sustainable Wellness</h5>
+                                    <div class="pq-fancy-box-content">
+                                        <p class="pq-fancybox-description">At Plum Wellness, we are not just about quick
+                                            fixes; we focus on sustainable lifestyle changes that lead to lasting
+                                            results. We celebrate every small victory along the way and provide ongoing
+                                            support to keep you motivated. Our community is built on encouragement,
+                                            sharing successes, and learning from challenges together.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pq-fancy-box-4 pq-fancy-bg-grey text-center ">
+                                <div class="pq-fancy-box-info">
+                                    <h5 class="pq-fancy-box-title">Transformative <br>Health Journeys</h5>
+                                    <div class="pq-fancy-box-content">
+                                        <p class="pq-fancybox-description">Your health journey is personal, and we are
+                                            honored to be part of it. Our clients often share stories of transformation,
+                                            from improved energy levels and enhanced mood to significant weight loss and
+                                            better stress management. These stories inspire us and reaffirm our
+                                            commitment to making a positive impact in the lives of others.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 include('web/footer.php');
