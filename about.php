b4007398a9a68a9e59ef36117de1b5ecd9f80470
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-10-10 11:25:14 +0530

    Update about.php

--- about.php
+++ about.php
@@ -35,7 +35,103 @@ include('web/header.php');
         </div>
     </div>
 </section>
-
+<!-- <project-start> -->
+<section class="protfolio p-0  pq-grey-bg-color">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col-lg-12 col-md-12 col-xl-4 pq-project-description">
+                <div class="pq-section-title-style-1 ">
+                    <h5 class="pq-section-title">Our Team</h5>
+                    <p class="pq-section-description">Our team comprises seasoned professionals, including experienced doctors and registered dietitians, all committed to your success. Together, we have helped over 10,000 individuals transform their lives through our evidence-based methodologies. We pride ourselves on fostering a supportive and inclusive environment, where your health and wellness goals are our top priority.</p>
+                </div>
+                <!-- <div class="pq-button-container pq-about-main-btn">
+                    <a class="pq-button pq-btn-flat" href="portfolio-single.html">
+                        <span class="pq-btn-text">
+                            <img src="images/btn-img.svg" alt="svg-image">read more</span>
+                        <div class="pq-btn-cover">
+                            <span class="pq-btn-text pq-btn-cover-text">
+                                <img src="images/btn-img.svg" alt="svg-image">read more</span>
+                            <div class="pq-btn-cover-bgrnd"></div>
+                        </div>
+                    </a>
+                </div> -->
+            </div>
+            <div class="col-lg-12 col-md-12 col-xl-8 p-0 pq-mt-60">
+                <div class="pq-main-portfoliobox-1 pq-bg-primary">
+                    <div class="pq-main-images-box">
+                        <div class="owl-carousel owl-loaded owl-drag" data-dots="false" data-nav="false" data-desk_num="3"
+                            data-lap_num="3" data-tab_num="2" data-mob_num="1" data-mob_sm="1" data-autoplay="true"
+                            data-loop="true" data-margin="30">
+                            <div class="item">
+                                <div class="pq-portfoliobox-1">
+                                    <div class="pq-portfolio-block">
+                                        <div class="pq-portfolio-img  ">
+                                            <img src="images/team/1234.jpg" alt="images">
+                                        </div>
+                                        <div class="pq-portfolio-info">
+                                            <h5 class="pq-blog-title txt-center">Dt. Miksha Arora</h5>
+                                            <div class="pq-testimonial-meta txt-center">
+                                                <h6 class="ad-title-text">Weight Loss Expert</h6> <span>10+ Years of Experience</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="pq-portfoliobox-1">
+                                    <div class="pq-portfolio-block">
+                                        <div class="pq-portfolio-img  ">
+                                            <img src="images/team/b.jpg" alt="images">
+                                        </div>
+                                        <div class="pq-portfolio-info">
+                                            <h5 class="pq-blog-title txt-center">Dt. Bhawna </h5>
+                                            <div class="pq-testimonial-meta txt-center">
+                                                <h6 class="ad-title-text">Disease Management Expert</h6><span>5+ Years of
+                                                    Experience</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="pq-portfoliobox-1">
+                                    <div class="pq-portfolio-block">
+                                        <div class="pq-portfolio-img  ">
+                                            <img src="images/team/123.jpg" alt="images">
+                                        </div>
+                                        <div class="pq-portfolio-info">
+                                            <h5 class="pq-blog-title txt-center">Dr. Ishika </h5>
+                                            <div class="pq-testimonial-meta txt-center">
+                                                <h6 class="ad-title-text">Skin Expert</h6><span>7+ Years of
+                                                    Experience</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="pq-portfoliobox-1">
+                                    <div class="pq-portfolio-block">
+                                        <div class="pq-portfolio-img  ">
+                                            <img src="images/team/a.jpg" alt="images">
+                                        </div>
+                                        <div class="pq-portfolio-info">
+                                            <h5 class="pq-blog-title txt-center">Dt. Akansha </h5>
+                                            <div class="pq-testimonial-meta txt-center">
+                                                <h6 class="ad-title-text">Weight Loss Expert</h6><span>8+ Years of Experience</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- <project-end> -->
 <?php
 include('web/footer.php');
 // include('web/hero-section-stress.php');
